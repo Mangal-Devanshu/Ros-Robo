d26e8c9f576954cbf0a322c8cee1f7ca0a43b09b
Mangal-Devanshu <sarojmangal7600@gmail.com>
2025-09-19 23:47:37 +0530

    Hardware Interface working

--- test/launch/launch_sim.launch.py
+++ test/launch/launch_sim.launch.py
@@ -39,15 +39,14 @@ def generate_launch_description():
     #         remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
     #     )
 
-    # gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
+    gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
 
     # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-                   
-             ) # launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
-
+                    launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
+             )
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
                         arguments=['-topic', 'robot_description',
@@ -55,17 +54,17 @@ def generate_launch_description():
                         output='screen')
 
 
-    # diff_drive_spawner = Node(
-    #     package="controller_manager",
-    #     executable="spawner.py",
-    #     arguments=["diff_cont"],
-    # )
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_cont"],
+    )
 
-    # joint_broad_spawner = Node(
-    #     package="controller_manager",
-    #     executable="spawner.py",
-    #     arguments=["joint_broad"],
-    # )
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad"],
+    )
 
 
     # Code for delaying a node (I haven't tested how effective it is)
@@ -89,12 +88,12 @@ def generate_launch_description():
     # Launch them all!
     return LaunchDescription([
         rsp,
-        
         gazebo,
-        spawn_entity
+        spawn_entity,
+        diff_drive_spawner,
+        joint_broad_spawner,
         
     ])# joystick,
         # twist_mux,# joystick,
         # twist_mux,
-        # diff_drive_spawner,
-        # joint_broad_spawner
+      
